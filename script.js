aeb0db00501c1a2341a79d41a716644e53a68fd4
Mohamad Hijazi <hijazimohamad@hotmail.com>
2019-07-08 23:30:35 +0300

    Adding starting project

---
+++ script.js
@@ -0,0 +1,16 @@
+var css=document.querySelector("h3");
+var col1=document.querySelector(".color1");
+var col2=document.querySelector(".color2");
+var body=document.getElementById("gradient");
+
+function setGradient(){
+    body.style.background="linear-gradient(to right, " + col1.value + ", " +col2.value +")"
+    css.textContent = body.style.background + ";";
+}
+
+
+
+
+col1.addEventListener("input", setGradient);
+
+col2.addEventListener("input",setGradient);
